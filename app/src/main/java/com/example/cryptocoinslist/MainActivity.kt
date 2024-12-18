e107b9e6f2d4d4f9bbe228e056aa623f3c271e12
ana-znv <uzanzaev44@gmail.com>
2024-12-18 21:34:37 +0600

    init

---
+++ app/src/main/java/com/example/cryptocoinslist/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.cryptocoinslist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cryptocoinslist.ui.theme.CryptoCoinsListTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CryptoCoinsListTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Main(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Main(modifier: Modifier = Modifier) {
+
+}
